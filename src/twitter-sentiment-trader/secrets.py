38fb867c012fd13c994ba9f11c7a99af858db1bc
Patrick Pfenning <patrick.pfenning@uptake.com>
2022-08-07 21:05:40 -0400

    project structure

---
+++ src/twitter-sentiment-trader/secrets.py
@@ -0,0 +1,17 @@
+import os
+
+"""
+API for finnhub.io: https://finnhub.io/docs/api
+"""
+FINNHUB_KEY = os.environ['FINNHUB_KEY']
+FINNHUB_SANDBOX = os.environ['FINNHUB_SANDBOX']
+"""
+API for Twitter: 
+"""
+TWITTER_KEY = os.environ['TWITTER_KEY']
+TWITTER_SECRET = os.environ['TWITTER_SECRET']
+"""
+API for Alpaca: https://alpaca.markets/docs/python-sdk/
+"""
+ALPACA_KEY = os.environ['ALPACA_KEY']
+ALPACA_SECRET = os.environ['ALPACA_SECRET']
